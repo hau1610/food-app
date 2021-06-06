1e3de4be0c2d402b71901ff82985b0304f55817a
trunghau <trunghautran1610@gmail.com>
2021-06-06 19:35:07 +0700

    Comment

--- lib/ingredient.dart
+++ lib/ingredient.dart
@@ -4,6 +4,7 @@ import 'package:food_app/fake_data.dart';
 import 'food.dart';
 
 class Ingredient extends StatelessWidget {
+  //Hello
   Food food;
 
   Ingredient({this.food});
@@ -17,38 +18,47 @@ class Ingredient extends StatelessWidget {
         ),
       ),
       body: Center(
-        child:food.ingredients != null?
-        Column(
-          children: <Widget>[
-            FadeInImage.assetNetwork(
-                placeholder: 'assets/images/id-loading-1.gif',
-                image: food.urlImage),
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Text(
-                "Ingredient of ${this.food.name}",
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.redAccent,
-                    fontWeight: FontWeight.bold),
+        child: food.ingredients != null
+            ? Column(
+                children: <Widget>[
+                  FadeInImage.assetNetwork(
+                      placeholder: 'assets/images/id-loading-1.gif',
+                      image: food.urlImage),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "Ingredient of ${this.food.name}",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.redAccent,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Expanded(
+                      child: ListView.builder(
+                          itemCount: food.ingredients.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              leading: CircleAvatar(
+                                backgroundColor: Colors.blue,
+                                child: Text(
+                                  '${index + 1}',
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.black),
+                                ),
+                              ),
+                              title: Text(
+                                food.ingredients[index],
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            );
+                          }))
+                ],
+              )
+            : Text(
+                "No Ingredient Found",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
               ),
-            ),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: food.ingredients.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        leading: CircleAvatar(backgroundColor: Colors.blue,
-                          child: Text('${index + 1}',style: TextStyle(fontSize: 18,color: Colors.black),),),
-                        title: Text(
-                          food.ingredients[index],
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      );
-                    }))
-          ],
-        )
-            :Text("No Ingredient Found",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
       ),
     );
   }
